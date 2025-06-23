19e5becc38c278c0544ae2b2620c643d5ad78e37
victolero.elton <victolero.elton@gmail.com>
2025-06-24 07:05:19 +0800

    Returned listen

--- index.js
+++ index.js
@@ -14,23 +14,20 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-	origin: ["http://localhost:3000", "https://blogapi-3cr8.onrender.com"]
+	origin: ["http://localhost:8000", "https://blogapi-3cr8.onrender.com"]
 }));
 
 // [SECTION] Database Connection
-mongoose.connect(process.env.MONGODB_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_STRING);
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'))
 
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
-// if(require.main === module){
-// 	app.listen(process.env.PORT || 4000, () => {
-// 	    console.log(`API is now online on port ${ process.env.PORT || 4000 }`)
-// 	});
-// }
+if(require.main === module){
+	app.listen(process.env.PORT || 4000, () => {
+	    console.log(`API is now online on port ${ process.env.PORT || 4000 }`)
+	});
+}
 	
 module.exports = {app,mongoose};
